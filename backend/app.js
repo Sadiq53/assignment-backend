48632347b2a01994e4b30aa3f96101680da0d5dc
Sadiq53 <jafarussadiq098@gmail.com>
2024-05-05 17:12:24 +0530

    changes"

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended : true }))
 app.use(cors());
 app.use(routes);
 
-let port = 8080;
+const PORT = process.env.PORT || 8080
 app.listen(port, (req, res)=>{
-    console.log("server running with port", port)
+    console.log("server running with port", PORT)
 });
